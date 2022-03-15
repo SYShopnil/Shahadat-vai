0dee112c25eac62e8071bb2637f4c545487388d3
Sadmaney Yeasar <1730034@iub.edu.bd>
2022-03-16 00:43:29 +0600

    first class

---
+++ class.js
@@ -0,0 +1,36 @@
+// console.log ("hello world")
+// console.log ("hello world")
+// console.log ("hello world")
+// console.log ("hello world")
+// console.log ("hello world")
+// console.log ("hello world")
+// console.log ("hello world")
+// console.log ("hello world")
+// console.log ("hello world")
+// console.log ("hello world")
+// var firstVar = "bye world";
+// console.log (firstVar)
+// console.log (firstVar)
+// console.log (firstVar)
+// console.log (firstVar)
+// console.log (firstVar)
+// console.log (firstVar)
+// console.log (firstVar)
+
+///string declaration  "" '' `` (backtick)
+// "232sfsdf4"
+// 'sjflksjafljasr32fsdfs' 
+// `sjdflkjsadflkjsdlfkjsadlf`
+// var firstVar = "232sfsdf4"
+// var firstVar = '232sfsdf4'
+// var firstVar = '232sfsdf4';
+// console.log (typeof firstVar)
+// console.log ("hello world " + 100) //hello world 100
+
+//method => () // property => () is not available
+var stringOne = "hello world";
+console.log (stringOne.toUpperCase()) //output: "HELLO WORLD"
+
+
+
+
